d98d23d228ca3f42aba60382d838b22ceebae8c7
陈修宏 <chenxh.cz@gmail.com>
2017-11-01 20:30:30 +0800

    格式化代码

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,7 @@ from .models import Article
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'content', 'pub_time')
-    list_filter = ('pub_time', )
+    list_filter = ('pub_time',)
+
 
 admin.site.register(Article, ArticleAdmin)
